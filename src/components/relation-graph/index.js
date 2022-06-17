c09b88db166c7c615ccfd314437291f60d9300a4
jack13163 <18163132129@163.com>
2022-06-17 19:32:52 +0800

    注册本地relation-graph

--- src/components/relation-graph/index.js
+++ src/components/relation-graph/index.js
@@ -1,6 +1,9 @@
 import RelationGraph from './index.vue'
-RelationGraph.install = function(Vue) {
-    Vue.component('relation-graph', RelationGraph);
-    Vue.component('seeks-relation-graph', RelationGraph);
-};
-export default RelationGraph
+
+const comment = {
+    install: function (Vue) {
+        Vue.component('relation-graph', RelationGraph);
+        Vue.component('seeks-relation-graph', RelationGraph);
+    }
+}
+export default comment
