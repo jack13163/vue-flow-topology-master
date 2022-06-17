f9cdc3ff5cbc23b7ff58d700c53589ad387c45ca
jack13163 <18163132129@163.com>
2022-06-17 20:04:48 +0800

    补充示例

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import VueRouter from 'vue-router'
 import Index from '../views/Index.vue'
 import Process from '../views/Process.vue'
 import RelationGraph from '../components/relation-graph/demo/Demo4Graph.vue'
+import Demo4LayoutTree from '../components/relation-graph/demo/Demo4LayoutTree.vue'
+import Demo4LayoutTree2 from '../components/relation-graph/demo/Demo4LayoutTree2.vue'
+import Demo4LayoutForce from '../components/relation-graph/demo/Demo4LayoutForce.vue'
+import Demo4LayoutCenter from '../components/relation-graph/demo/Demo4LayoutCenter.vue'
 
 Vue.use(VueRouter)
 
@@ -29,6 +33,31 @@ const routes = [
     path: '/graph',
     name: 'RelationGraph',
     component: RelationGraph
+  },
+  {
+    path: '/graph/center',
+    name: 'RelationGraph',
+    component: RelationGraph
+  },
+  {
+    path: '/graph/force',
+    name: 'Demo4LayoutForce',
+    component: Demo4LayoutForce
+  },
+  {
+    path: '/graph/center',
+    name: 'Demo4LayoutCenter',
+    component: Demo4LayoutCenter
+  },
+  {
+    path: '/graph/tree',
+    name: 'Demo4LayoutTree',
+    component: Demo4LayoutTree
+  },
+  {
+    path: '/graph/tree2',
+    name: 'Demo4LayoutTree2',
+    component: Demo4LayoutTree2
   }
 ]
 
