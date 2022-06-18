4ad926acc8bf65788cd5e824a1b5b1007b37cdde
jack13163 <2463246583@qq.com>
2022-06-18 23:13:03 +0800

    添加自动布局功能

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -49,6 +49,8 @@ import HeaderAttr from "@/components/modules/HeaderAttr";
 import FlowMenu from "@/components/FlowMenu";
 import FlowAttr from "@/components/FlowAttr";
 import FlowContent from "@/components/FlowContent";
+import SeeksRGLayouters from "@/components/relation-graph/core4vue/SeeksRGLayouters"
+import SeeksRGUtils from '@/components/relation-graph/core4vue/SeeksRGUtils'
 
 export default {
   name: "Index",
@@ -64,7 +66,7 @@ export default {
       isMouseDownStop: false,
       isJsonView: false,
       jsonData: "暂无内容",
-      graphOptions: {
+      graphSetting: {
         defaultNodeBorderWidth: 0,
         allowSwitchLineShape: true,
         allowSwitchJunctionPoint: true,
@@ -78,6 +80,13 @@ export default {
         ],
         defaultJunctionPoint: 'border'
         // 这里可以参考"Graph 图谱"中的参数进行设置
+      },
+      graphData: {
+        nodes: [],
+        lines: [],
+        rootNode: null,
+        nodes_map: {},
+        lines_map: {}
       }
     };
   },
@@ -110,6 +119,8 @@ export default {
         this.$refs.operate.handleMiddleMenu(type);
       } else if (type === "auto-layout") {
         console.log("自动布局...")
+        this.setJsonData(this.jsonData)
+        this.toggleAutoLayout()
       }
     },
     handleCloseJsonView() {
@@ -131,10 +142,108 @@ export default {
       }
     },
     // 自动布局相关逻辑
-    switchLayout(layoutConfig) {
-      if (window.SeeksGraphDebug) console.log('change layout:', layoutConfig)
-      SeeksRGLayouters.switchLayout(layoutConfig, this.graphSetting)
-      this.refresh()
+    setJsonData(jsonData) {
+      this.viewSizeIsInited = true
+      this.nodeViewList = []
+      this.lineViewList = []
+      this.graphData.nodes = []
+      this.graphData.lines = []
+      this.graphData.nodes_map = {}
+      this.graphData.lines_map = {}
+      this.graphData.rootNode = null
+      console.log('set jsonData::', jsonData)
+      if (this.graphSetting.layouts && this.graphSetting.layouts.length > 0) {
+        var _defaultLayoutSetting = this.graphSetting.layouts[0]
+        if (window.SeeksGraphDebug) console.log('创建默认布局器：', this.graphSetting.layoutName)
+        if (_defaultLayoutSetting.layouter) {
+          this.graphSetting.layouter = _defaultLayoutSetting.layouter
+        } else {
+          this.graphSetting.layouter = SeeksRGLayouters.createLayout(_defaultLayoutSetting, this.graphSetting)
+        }
+      } else {
+        console.log('你需要设置layouts来指定当前图谱可以使用的布局器！')
+      }
+      var __root_id = jsonData['rootId']
+      this.loadGraphJsonData(jsonData)
+      console.log('graphData:', this.graphData)
+      if (__root_id) {
+        this.graphData.rootNode = this.graphData.nodes_map[__root_id]
+      }
+      if (!this.graphData.rootNode && this.graphData.nodes.length > 0) {
+        this.graphData.rootNode = this.graphData.nodes[0]
+      }
+      this.applyNewDataToCanvas()
+      if (this.graphSetting.layouter && this.graphData.rootNode) {
+        console.log('需要布局的节点数量：', this.graphData.nodes.length)
+        this.graphSetting.layouter.placeNodes(this.graphData.nodes, this.graphData.rootNode, this.graphSetting)
+      }
+    },
+    applyNewDataToCanvas() {
+      this.graphData.nodes.forEach(thisNode => {
+        if (thisNode.appended === false) {
+          thisNode.appended = true
+          this.nodeViewList.push(thisNode)
+        }
+      })
+      this.graphData.lines.forEach(thisLine => {
+        if (thisLine.appended === false) {
+          thisLine.appended = true
+          this.lineViewList.push(thisLine)
+        }
+      })
+      if (!this.graphData.rootNode) {
+        this.graphData.rootNode = this.graphData.nodes[0]
+      }
+      if (this.graphSetting.defaultFocusRootNode) {
+        this.graphSetting.checkedNodeId = this.graphData.rootNode.id
+      }
+    },
+    loadNodes(_nodes) {
+      _nodes.forEach(thisNodeJson => {
+        let thisNode = SeeksRGUtils.json2Node(thisNodeJson)
+        let __isNew = false
+        if (this.graphData.nodes_map[thisNode.id]) {
+          thisNode = this.graphData.nodes_map[thisNode.id]
+        } else {
+          __isNew = true
+        }
+        if (__isNew) {
+          this.graphData.nodes_map[thisNode.id] = thisNode
+          this.graphData.nodes.push(thisNode)
+          thisNode.seeks_id = this.seeksNodeIdIndex++
+          thisNode.appended = false
+        }
+      })
+    },
+    loadGraphJsonData(jsonData) {
+      // 兼容以前的配置
+      if (!jsonData.links) jsonData.links = jsonData.lines
+      if (!jsonData.links) jsonData.links = jsonData.relations
+      var _orign_nodes = jsonData.nodes
+      var _nodes = []
+      var _links = []
+      this.flatNodeData(_orign_nodes, null, _nodes, _links)
+      jsonData.links = _links.concat(jsonData.links)
+      this.loadNodes(_nodes)
+      console.log('节点预处理完毕')
+    },
+    flatNodeData(orign_nodes, parentNode, nodes_collect, links_collect) {
+      orign_nodes.forEach(thisOrignNode => {
+        if (!thisOrignNode.flated) {
+          thisOrignNode.flated = true
+          nodes_collect.push(thisOrignNode)
+          if (parentNode) {
+            links_collect.push({
+              from: parentNode.id,
+              to: thisOrignNode.id,
+            })
+          }
+          var _childs = thisOrignNode.childs || thisOrignNode.children
+          if (_childs && _childs.length > 0) {
+            this.flatNodeData(_childs, thisOrignNode, nodes_collect, links_collect)
+          }
+        }
+      })
     },
     toggleAutoLayout() {
       this.graphSetting.autoLayouting = !this.graphSetting.autoLayouting
@@ -151,16 +260,6 @@ export default {
         } else {
           this.graphSetting.layouter.stop()
         }
-      }
-    },
-    printGraphJsonData() {
-      this.hits++
-      setTimeout(() => {
-        if (this.hits > 0) this.hits--
-      }, 2000)
-      if (this.hits > 5) {
-        this.hits = 0
-        this.$parent.printGraphJsonData()
       }
     }
   },
